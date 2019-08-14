5ffae6c6590b7ffaddc294181fbba0ae1be8f155
Kyle Clemons <cl3m0ns@gmail.com>
2019-08-13 22:38:39 -0400

    throwing enemy added

---
+++ Bullet/EnemyBullet.gd
@@ -0,0 +1,48 @@
+extends KinematicBody2D
+
+var TYPE = "ENEMY_BULLET"
+const speed = 50
+var moveDir = Vector2.ZERO
+var canHurt = true
+var deathAnim = preload("res://Explosion/EnemyBulletExplosion.tscn")
+var damage = 0
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_z_index(1)
+	explode()
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	var movement = moveDir.normalized() * speed * delta
+	var collision_info = move_and_collide(movement)
+	if collision_info:
+		explode_and_die()
+	
+	damage_loop()
+	
+	if $LifeTimer.is_stopped():
+		queue_free()
+		
+
+func explode_and_die():
+	explode()
+	queue_free()
+
+func explode():
+	var boom = deathAnim.instance()
+	boom.set_position(position)
+	get_parent().add_child(boom)
+
+func damage_loop():
+	for body in $Hitbox.get_overlapping_bodies():
+		if body.get("TYPE") == "PLAYER" && canHurt:
+			print("hurting someone")
+			body.knockDir = body.get_global_position() - get_global_position()
+			body.take_damage()
+			canHurt = false
+			explode_and_die()
---
+++ Enemies/ThrowingEnemy.gd
@@ -0,0 +1,108 @@
+extends KinematicBody2D
+var TYPE = "ENEMY"
+
+enum {
+	IDLE,
+	ATTACK
+}
+var hp = 3
+var player = null
+var damage = 1
+var throwingCooldown = 0
+var stateChooser = [IDLE, ATTACK]
+var deathName = "Throwing1"
+var iframes = 0
+#resources
+var bloodSplatter = preload("res://Explosion/DeathSplatter.tscn")
+var death = preload("res://Enemies/DeadEnemies.tscn")
+var bullet = preload("res://Bullet/EnemyBullet.tscn")
+var state = IDLE
+
+func _ready():
+	set_physics_process(false)
+	set_z_index(1)
+	if get_parent().has_node("Player"):
+		player = get_parent().get_node("Player")
+	$AnimationPlayer.play('idle')
+	
+
+func _physics_process(delta):
+	if iframes > 0:
+		iframes -= 1
+		get_node("Sprite").modulate = Color(10,10,10,10)
+	else:
+		get_node("Sprite").modulate = Color(1,1,1,1)
+	
+	do_state()
+
+func do_state():
+	if throwingCooldown > 0:
+		throwingCooldown -= 1
+
+	
+	match state:
+		IDLE:
+			$AnimationPlayer.play('idle')
+		ATTACK:
+			do_attack()
+
+
+func do_bullet():
+	var myBullet = bullet.instance()
+	var bulletPos = Vector2.ZERO
+	var bulletDir = Vector2.RIGHT
+	match $Sprite.flip_h:
+		true:
+			bulletPos = Vector2(position.x -10, position.y + 2)
+			bulletDir = Vector2.LEFT
+			myBullet.set_rotation_degrees(180)
+		false:
+			bulletPos = Vector2(position.x +10, position.y + 2)
+			bulletDir = Vector2.RIGHT
+			myBullet.set_rotation_degrees(0)
+	
+	myBullet.moveDir = bulletDir
+	myBullet.damage = damage
+	myBullet.set_position(bulletPos)
+	
+	#add to scene
+	get_parent().add_child(myBullet)
+
+func do_attack():
+	if throwingCooldown <= 0:
+		$AnimationPlayer.play("attack")
+		throwingCooldown = 100
+
+func choose(array):
+	array.shuffle()
+	return array.front()
+
+func _on_StateTimer_timeout():
+	$StateTimer.wait_time = choose([0.25, 0.5, 0.75])
+	if throwingCooldown <= 0:
+		state = choose(stateChooser)
+	elif state != ATTACK:
+		state = IDLE
+
+func take_damage():
+	if iframes == 0:
+		iframes = 15
+		hp -= 1
+		if hp <= 0:
+			do_death()
+
+func do_death():
+	get_node("Sprite").modulate = Color(10,10,10,10)
+	var boom = bloodSplatter.instance()
+	boom.set_position(position)
+	get_parent().add_child(boom)
+	var dead = death.instance()
+	dead.set_position(position)
+	dead.deathName = "Throwing1"
+	get_parent().get_node("DeadEnemies").add_child(dead)
+	queue_free()
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "attack":
+		do_bullet()
+		state = IDLE
