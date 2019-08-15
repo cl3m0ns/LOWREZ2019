de4d205cf96d984fc7e6430705bb82606b4a579d
Kyle Clemons <cl3m0ns@gmail.com>
2019-08-14 22:47:04 -0400

    big boi update

---
+++ Player/Heart_Pickup.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play('move')
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	for body in $Hitbox.get_overlapping_bodies():
+		if body.get("TYPE") == "PLAYER":
+			if body.hp != body.maxHp:
+				body.hp += 1
+			get_parent().find_node("Camera").update_hp()
+			queue_free()
