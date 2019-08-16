ed2b4ef83c18c8ba51d18c212ca1eb6188969ae6
Kyle Clemons <cl3m0ns@gmail.com>
2019-08-15 22:51:18 -0400

    boss added not done

--- Player/Heart_Pickup.gd
+++ Player/Heart_Pickup.gd
@@ -5,7 +5,6 @@ var TYPE = "PICKUP"
 # var b = "text"
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimationPlayer.play('move')
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
